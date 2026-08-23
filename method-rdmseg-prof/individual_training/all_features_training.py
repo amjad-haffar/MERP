11f5cc002d166d5c5d40974949378a358a46c766
amjad-alhaffar <amjad.alhaffar.aa@gmail.com>
2026-08-23 14:10:01 +0100

    simple fusion again

--- method-rdmseg-prof/individual_training/all_features_training.py
+++ method-rdmseg-prof/individual_training/all_features_training.py
@@ -31,6 +31,7 @@ from dataset import rdm_dataset as dataset_class
 from networks import Three_FC_layer as archi_linear
 from networks import lstm_double_late_profile_branch as late_fusion
 from networks import lstm_double_late_middle as late_fusion_middle
+from networks import lstm_double_late_simple as late_fusion_simple
 
 
 #####################
@@ -275,7 +276,7 @@ if __name__ == "__main__":
     else:
         setattr(args, 'model_name', f'{args.affect_type[0]}_p_{args.model_name}')
         exp_log_filepath = os.path.join(dir_path,save_models_foldername,'test_log_lstm.pkl')
-        archi = late_fusion_middle
+        archi = late_fusion_simple
     print(args)
 
     # check if folder with same model_name exists. if not, create folder.
